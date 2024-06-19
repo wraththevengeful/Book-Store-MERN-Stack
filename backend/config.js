bc6f81d86b07255bbef06d431027aeff11d6540b
WrathOnDebian <elanchezhiyan2440@gmail.com>
2024-06-19 19:39:10 +0530

    Backing up

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://2023178047ceg:yJ8iJoKARGmsG92F@personal-library.vruhwaw.mongodb.net/?retryWrites=true&w=majority&appName=personal-library';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
